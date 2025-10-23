3c2ff3bf18c88ba533e441bb5888ba3da2cdd3f4
willlkhang <minhkhangnguyen31104@gmail.com>
2025-10-23 12:30:16 +0800

    clean the wildard_hander.c

--- wildcard_handler.h
+++ wildcard_handler.h
@@ -2,6 +2,7 @@
 #define WILDCARD_HANDLER_H
 
 int has_wildcard(const char* str);
+void add_arg_to_list(char ***new_argv_ptr, int *count, int *capacity, const char *arg_to_add) ;
 char** expand_wildcards(char** argv);
 void free_expanded_argv(char** argv);
 
